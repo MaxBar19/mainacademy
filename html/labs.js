2e8380b5abc5d40ae6e81d9cebe4ab91f2c993ae
Max Kovalenko <justspook42@gmail.com>
2018-11-05 17:13:36 +0200

    Fourth lab

--- html/labs.js
+++ html/labs.js
@@ -1,19 +1,29 @@
+//_____lab4_____//
+
+function value(a) {
+    return function(b){return a*b}
+}
+var mul = value(3);
+console.log(mul(4));
 //_____lab3_____//
+/*
 function num_modifier(num) {
     if (num>10) return Math.pow(num, 2)
     else if (num<7) return console.log("The number is lower than 7")
     else if (num==8 || num==9) return num - 1
 }
 console.log(num_modifier(11));
-
+*/
 //____lab2_____//
+/*
 function length_count(arr) {
     if(arr == undefined) console.log("Wrong argument exception")
     else return arr.length;
 }
 console.log(length_count());
-
+*/
 //___lab1____//
+/*
 function object_creator(countries, population) {
 
 var pops_in_countries = {};
@@ -29,3 +39,4 @@ return pops_in_countries;
 }
 
 object_creator(["12","23","43"], [12,23,43]);
+*/
